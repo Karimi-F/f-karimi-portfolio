41fbd87bcc3aaef709799b36bf1f3bf6705195c6
Karimi-F <fridah37karimi@gmail.com>
2025-07-10 09:18:34 +0300

    Modifies the nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,25 @@
-"use client";
+'use client';
 
-export default function NavBar (){
-    return(
-        <div>
+import Link from 'next/link';
 
-        </div>
-    )
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center sticky top-0 z-50 px-6 py-4 bg-white shadow-md text-black">
+        {/* <nav className="sticky top-0 z-50  > */}
+      <h1 className="text-2xl font-bold">
+        Karimi F's Portfolio
+          </h1>
+      <div className="text-xl font-bold text-red-500">
+        
+      </div>
+
+      <div className="flex gap-6 text-sm md:text-base">
+        <Link href="/" className="hover:underline text-black hover:text-red-500">Home</Link>
+        <Link href="/about" className="hover:underline text-black hover:text-red-500">About</Link>
+        <Link href="/contact" className="hover:underline text-black hover:text-red-500">Contact</Link>
+
+        
+      </div>
+    </nav>
+  );
 }
